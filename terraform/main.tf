8f29ccd199bfb36e7e3c90663e8e22344ff85b2d
Vorn saran <v.saran@dgc.gov.kh>
2026-07-06 00:13:17 +0700

    Update Terraform configuration to include AWS provider settings, adjust key pair public key path, and format resource definitions for improved readability.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,10 @@
+provider "aws" {
+  region = var.aws_region
+}
+
 resource "aws_key_pair" "food" {
-
-  key_name = var.key_name
-
-  public_key = file(var.public_key_path)
-
+  key_name   = var.key_name
+  public_key = file("${path.module}/keys/food-key.pub")
 }
 
 resource "aws_security_group" "food" {
@@ -11,57 +12,35 @@ resource "aws_security_group" "food" {
   name = "foodexpress"
 
   ingress {
-
-    from_port = 22
-
-    to_port = 22
-
-    protocol = "tcp"
-
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-
   }
 
   ingress {
-
-    from_port = 3000
-
-    to_port = 3000
-
-    protocol = "tcp"
-
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-
   }
 
   egress {
-
-    from_port = 0
-
-    to_port = 0
-
-    protocol = "-1"
-
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-
   }
-
 }
 
 resource "aws_instance" "food" {
 
-  ami = var.ami
-
-  instance_type = var.instance_type
-
-  key_name = aws_key_pair.food.key_name
-
+  ami                    = var.ami
+  instance_type          = var.instance_type
+  key_name               = aws_key_pair.food.key_name
   vpc_security_group_ids = [aws_security_group.food.id]
 
   tags = {
-
     Name = "FoodExpress"
-
   }
-
 }
